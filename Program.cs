d72fbeeee19743f62cb8a146d9f7c834d63ef3da
farrellw <farrell.will.g@gmail.com>
2016-12-23 10:05:04 -0600

    Add binary search functionality.
    Create fibonnacci.cs form

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            
+            int[] numbers = { 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60 };
+            int[] numbersTwo = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20 };
+            int returnedOne = ConsoleApplication.BinarySearch.searcher(numbers, 55, 0, numbers.Length);
+            Console.WriteLine(returnedOne);
+            //int returnedTwo = ConsoleApplication.BinarySearch.searcher(numbersTwo, 3, 0, numbersTwo.Length);
+            //Console.WriteLine(returnedTwo);
         }
 
         public static string Writer(string variable)
---
+++ binarysearch.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+
+namespace ConsoleApplication 
+{
+    public class BinarySearch {
+
+        public static int searcher(int[] inputArray, int inputInt, int min, int max)
+        {
+            //check the halfway point index 
+            int checkIndex = (min + max) / 2;
+            if (inputArray[checkIndex] > inputInt) {
+                return searcher(inputArray, inputInt, min, checkIndex);
+            } else if (inputArray[checkIndex] < inputInt) {
+                return searcher(inputArray, inputInt, checkIndex, max);
+            } else {
+                return checkIndex;
+            }
+        }
+
+    }
+
+}
